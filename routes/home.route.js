305def83a280e5c8e5ce5e7d0479a9cfb981c2ea
Bastian <bpt.bastian@gmail.com>
2024-04-19 13:58:52 -0400

    for deleted

--- routes/home.route.js
+++ routes/home.route.js
@@ -5,27 +5,25 @@ import moment from 'moment';
 import _ from 'lodash'
 import chalk from "chalk";
 
+let users = [];
 
 const router = Router();
 moment.locale('es')
 
 router.get('/', async (req, res) => {
-    let users = [];
 
-    for ( let i = 0; i <= 15; i++){
-        const {data} = await axios.get('https://randomuser.me/api/');
-        const user ={
-            firstName : data.results[0].name.first,
-            lastName  : data.results[0].name.last,
-            gender    : data.results[0].gender,
-            id        : nanoid(),
-            date      : moment().format('LLLL')
-        }
-        users = [...users, user]
+    const { data } = await axios.get('https://randomuser.me/api/');
+    const user = {
+        firstName: data.results[0].name.first,
+        lastName: data.results[0].name.last,
+        gender: data.results[0].gender,
+        id: nanoid(),
+        date: moment().format('LLLL')
     }
-    
-    const male    = _.filter(users, {gender : 'male'})
-    const female = _.filter(users, {gender : 'female'})
+    users = [...users, user]
+
+    const male = _.filter(users, { gender: 'male' })
+    const female = _.filter(users, { gender: 'female' })
 
     console.log(chalk.bgWhite.blue(users))
 
